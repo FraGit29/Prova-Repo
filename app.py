9c2f1e7c64deb66e4d14d7a700334be606c82e97
FraGit29 <francesco.furlan29@gmail.com>
2023-05-14 11:09:21 +0200

    creazione

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+def main():
+    st.write("testo prova")
+
+
+
+    
+if __name__ == "__main__":
+    main()
+#scrivere in terminale la prima volta : python -m streamlit run app.py 
